03ccbd331e53e3f83897a843c74567898fc3a295
ASG <ASG@8a1e3bcc-6fb5-8a45-9566-1e9252dc2308>
2008-05-01 12:20:32 +0000

    В Цвет добавлены константы. В Пересечение добавлена версия ПостроенияПересечения, приспособленная для работы с Материалом.
    git-svn-id: file:///WORK/finished-projects/svn-repositories/jproject-repository/Geopt/GeoptMathLib@64 8a1e3bcc-6fb5-8a45-9566-1e9252dc2308

--- src/geopt/colors/Color.java
+++ src/geopt/colors/Color.java
@@ -23,6 +23,18 @@ public class Color implements Serializable {
     private double green;
     private double blue;
     
+    // --- Константы ---
+    
+    public static final Color WHITE = new Color(1, 1, 1);
+    public static final Color BLACK = new Color(0, 0, 0);
+    public static final Color RED = new Color(1, 0, 0);
+    public static final Color GREEN = new Color(0, 1, 0);
+    public static final Color BLUE = new Color(0, 0, 1);
+    public static final Color YELLOW = new Color(1, 1, 0);
+    
+    
+    
+    
     // --- Конструкторы ---
     
     public Color() {
